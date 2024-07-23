b80183c8974f934467f87c7b7521c445e0c9355f
Paula Biazotto <pds_paula@hotmail.com>
2024-07-23 16:46:25 +0100

    feat: added execute.sh

---
+++ execute.sh
@@ -0,0 +1,22 @@
+docker build -t inception . >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+        echo "Inception part is builded âœ…"
+    else
+        echo "ERROR:"
+        echo "---"
+        echo " "
+        docker build -t inception .
+        echo " "
+        echo "---"
+        echo "Solve this problem, Inception is not builded âŒ"
+        exit 1
+    fi
+      
+echo "Launch with -it ? (y/n)"
+read answer
+if [ "$answer" = "y" ]; then
+    echo "Use 'exit' to stop container"
+    docker run -it inception
+else
+    docker run inception
+fi
