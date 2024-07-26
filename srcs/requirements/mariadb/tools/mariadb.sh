c42fa8e4bfe7f2b7d036ba05759c94d27552c25d
Paula Biazotto <pds_paula@hotmail.com>
2024-07-26 13:44:55 +0100

    chore: config mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,7 +2,7 @@
 
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXIST `${SQL_DATABASE} `;"
+mysql -e "CREATE DATABASE IF NOT EXISTS `${SQL_DATABASE} `;"
 
 mysql -e "CREATE USER IF NOT EXISTS `${SQL_USER}`@'localhost' IDENTIFIED BY `${SQL_PASSWORD}`;"
 
