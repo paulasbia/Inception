14ad709cffaf1790a1b73301139c4c87c3abb52f
Paula Biazotto <pds_paula@hotmail.com>
2024-07-31 09:14:57 +0100

    chore: starting wp.sh

---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,5 @@
+#https://www.linode.com/docs/guides/how-to-install-wordpress-using-wp-cli-on-debian-10/
+
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# sudo mv wp-cli.phar /usr/local/bin/wp
