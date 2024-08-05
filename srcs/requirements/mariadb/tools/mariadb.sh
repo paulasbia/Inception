f5b8ce3be60626c4b5c1c079a54b3d562b9e2584
Paula Biazotto <pds_paula@hotmail.com>
2024-08-05 14:17:30 +0100

    update mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-service mysql start;
+service mariadb start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 
-mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@localhost IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS ${SQL_USER}@localhost IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-mysql -e "FLUSH PRIVILEGES;"
+mariadb -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+echo "Stopping MariaDB..."
+sleep 2
+su root service mariadb stop
 
-exec mysqld_safe
+echo "Starting MariaDB in safe mode..."
+exec mysqld_safe --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
+
 #https://www.linode.com/docs/guides/how-to-install-wordpress-using-wp-cli-on-debian-10/
 
 # curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 # chmod +x wp-cli.phar
 # sudo mv wp-cli.phar /usr/local/bin/wp
 
-#!/bin/sh
 
 # WordPress installation
 if [ -f ./wp-config.php ]
@@ -15,7 +16,7 @@ else
 	echo "WordPress download"
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
-    sudo mv wp-cli.phar /usr/local/bin/wp
+    sudo mv wp-cli.phar /usr/local/bin/wordpress
 	
 	echo "WordPress configuration"
 	sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
