eb411cc81c69f7203a615d39dc52f910652a134d
Paula Biazotto <pds_paula@hotmail.com>
2024-08-06 17:15:44 +0100

    fix: wp config hostname and ../wordpress

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -19,14 +19,14 @@ else
     chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
 	
-	mv /wordpress/* .
+	mv ../wordpress/* .
 	rm -rf ../wordpress
 	cp wp-config-sample.php wp-config.php
 
 	echo "WordPress configuration"
 	sed -i "s/username_here/$DB_USER/g" wp-config.php
 	sed -i "s/password_here/$DB_PASSWORD/g" wp-config.php
-	sed -i "s/database_name_here/$DB_HOSTNAME/g" wp-config.php
+	sed -i "s/database_name_here/$DB_DATABASE/g" wp-config.php
 	sed -i "s/localhost/$DB_HOSTNAME/g" wp-config.php
     echo "Installing WordPress:"
 	wp core install --allow-root --url=${DOMAIN_NAME} --title="Inception" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
